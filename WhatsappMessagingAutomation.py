35bff2484e6b188a0f06503e11e0d4e2f342522e
SagarSoni-29 <sagar1983soni@gmail.com>
2025-11-29 16:17:56 +0530

    Whatsapp_Messaging_Automation
    This is the project of Whatsapp Messaging Automation . Here , we can send message to any of our known  , and we can also schedule it (on which time we want to send)...

---
+++ WhatsappMessagingAutomation.py
@@ -0,0 +1,3 @@
+import pywhatkit
+pywhatkit.sendwhatmsg_instantly("+917073443901","Hello , I have send this message througth Python, Thank you")
+pywhatkit.sendwhatmsg("+919034233114","Hello , I have send this message througth Python, Thank you",11,23)
